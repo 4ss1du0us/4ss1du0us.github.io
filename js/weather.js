15c38cbf79543c0381342cca33d0af596d6264e5
wyy-52 <916884674@qq.com>
2023-03-18 21:54:12 +0800

    Site updated: 2023-03-18 21:54:10

---
+++ js/weather.js
@@ -0,0 +1,10 @@
+WIDGET = {
+    "CONFIG": {
+      "layout": "1",
+      "width": "450",
+      "height": "150",
+      "background": "1",
+      "dataColor": "FFFFFF",
+      "key": "b5a84926f28141939e14d089e0f01adf"
+    }
+  }
