b8f1ae3434c811dfd36395787340ff25955ce664
wyy-52 <916884674@qq.com>
2023-03-22 17:05:35 +0800

    Site updated: 2023-03-22 17:05:29

--- js/ripples.js
+++ js/ripples.js
@@ -1,12 +1,1 @@
-$(document).ready(function () {
-    try {
-      $("#page-header").ripples({
-        resolution: 512,
-        dropRadius: 10, //px
-        perturbance: 0.001, //干扰度
-      });
-    } catch (e) {
-      $(".error").show().text(e);
-    }
-  });
-  
+$(document).ready((function(){try{$("#page-header").ripples({resolution:512,dropRadius:10,perturbance:.001})}catch(e){$(".error").show().text(e)}}));
